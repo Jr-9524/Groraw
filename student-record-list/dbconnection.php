661d0a9a6fa62bfb0a9bd9a65a3178e6f0c1d5d4
Jr-9524 <jhonruzzelolan2@gmail.com>
2024-11-18 21:32:41 +0800

    Add files via upload

---
+++ student-record-list/dbconnection.php
@@ -0,0 +1,13 @@
+<?php
+
+    $db_server = "localhost";
+    $db_user = "root";
+    $db_pass = "your_password";
+    $db_name = "record-list";
+    $conn = mysqli_connect($db_server,
+                            $db_user,
+                            $db_pass,
+                            $db_name);
+    
+                            
+?>
